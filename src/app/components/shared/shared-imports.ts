4eeee55761779500841ab0a0c562a2a4a2a9f65f
pappzsol <papp.zsolt.gabor@gmail.com>
2026-09-19 18:13:27 +0200

    prettier

--- src/app/components/shared/shared-imports.ts
+++ src/app/components/shared/shared-imports.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
 
 import { MessageComponent } from './message/message.component';
+import { PaginationComponent } from './pagination/pagination.component';
 import { BackButtonComponent } from '../shared/components/back-button/back-button.component';
 
 export const SHARED_IMPORTS = [
@@ -13,4 +14,5 @@ export const SHARED_IMPORTS = [
   TranslatePipe,
   MessageComponent,
   BackButtonComponent,
+  PaginationComponent,
 ];
