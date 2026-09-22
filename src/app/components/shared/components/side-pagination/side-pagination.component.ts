bf480ea69edd934bbf5dae4d822c5a6fa6d5b339
pappzsol <papp.zsolt.gabor@gmail.com>
2026-09-22 13:04:03 +0200

    comp

--- src/app/components/shared/components/side-pagination/side-pagination.component.ts
+++ src/app/components/shared/components/side-pagination/side-pagination.component.ts
@@ -10,10 +10,24 @@ import { LucideChevronLeft, LucideChevronRight } from '@lucide/angular';
   styleUrl: './side-pagination.component.css',
 })
 export class SidePaginationComponent {
-  @Input() currentPage = 1;
-  @Input() totalPages = 0;
+  @Input()
+  currentPage = 1;
 
-  @Output() pageChange = new EventEmitter<number>();
+  @Input()
+  totalPages = 0;
+
+  /**
+   * Mobil nézetben a kártya teljes rendelkezésre álló
+   * szélességét használja.
+   *
+   * Alapértelmezés: false, így a meglévő oldalak
+   * viselkedése változatlan marad.
+   */
+  @Input()
+  fullWidthMobile = false;
+
+  @Output()
+  pageChange = new EventEmitter<number>();
 
   onPreviousPage(): void {
     if (this.currentPage > 1) {
