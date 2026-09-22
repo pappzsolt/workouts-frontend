ed25bd8d707acdac674f649594eb24bb7ae40fa2
pappzsol <papp.zsolt.gabor@gmail.com>
2026-09-22 08:29:51 +0200

    comp

---
+++ src/app/components/shared/components/side-pagination/side-pagination.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-side-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './side-pagination.component.html',
+})
+export class SidePaginationComponent {
+  @Input() currentPage = 1;
+  @Input() totalPages = 0;
+
+  @Output() pageChange = new EventEmitter<number>();
+
+  onPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+
+  onNextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+}
